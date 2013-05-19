06ca637405628072ca9d34a8436f512b558b411b
Allen <atseng247@gmail.com>
2013-05-19 15:05:53 -0400

    constraints create

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,5 @@ class Author < ActiveRecord::Base
 
   validates_uniqueness_of :email
   validates_presence_of :email, :first_name, :last_name
+  validates_length_of :email, :minimum 7, message: "Email provdied does not contain enough characters."
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :author, :category, :content, :title
   validates_uniqueness_of :content, :title
+  validates_length_of :title, minimum: 3, message: "Post title needs to be greater than 3 characters."
 end
