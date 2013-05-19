9e760f79ced5b548acd2a4f484245db75258e329
Allen <atseng247@gmail.com>
2013-05-19 15:11:16 -0400

    fix model constraint syntax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   attr_accessible :author, :content, :post
 
   validates_presence_of :author, :content, :post
-  validates_length_of :content, maximum: 140, message "You have reached maximum character length of 140."
+  validates_length_of :content, maximum: 140, message: "You have reached maximum character length of 140."
 end
